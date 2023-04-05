44e0acba2de986ba8a92a84c146d3e19d8fe29b2
Otim Brian <otims.brian@gmail.com>
2023-04-06 01:00:05 +0300

    Finished Exercises 1.3.-1.5.

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,23 +1,57 @@
+import "./App.css"
 
-import './App.css';
-
-const Hello = ({name}) => {
+const Header = ({ course }) => {
   return (
-    <div>
-      <p>Hello world {name}</p>
+    <>
+      <h1>{course} </h1>
+      <br />
+    </>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      <h3>{parts[0].name} {parts[0].exercises}</h3>
+      <h3>{parts[1].name} {parts[1].exercises}</h3>
+      <h3>{parts[2].name} {parts[2].exercises}</h3>
+    </>
+  )
+}
+
+const Total = ({ parts }) => {
+  return (
+    <>
+      <p>{parts[2].exercises + parts[1].exercises + parts[0].exercises}</p>
+    </>
+  )
+}
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div className="App">
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
 
-function App() {
-  const now = new Date()
-  return (
-    <div className="App">
-      <p>Hello World {now.toString()}</p>
-
-      <Hello name="Otim" />
-    </div>
-  );
-}
-
-export default App;
+export default App
