c2f03e4192faa9ed412eb2931aad2326a8f8d97c
Otim Brian <otims.brian@gmail.com>
2023-04-05 20:50:57 +0300

    Finished Exercises 1.1

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,23 +1,47 @@
+import "./App.css"
 
-import './App.css';
-
-const Hello = ({name}) => {
+const Header = ({course}) => {
   return (
-    <div>
-      <p>Hello world {name}</p>
+    <>
+      <h1>{course} </h1>
+      <br/>
+    </>
+  )
+}
+
+const Content = ({content, exercise}) => {
+  return(
+    <>
+      <h3>{content} {exercise}</h3>
+    </>
+  )
+}
+
+const Total = (props) => {
+  return(
+    <>
+      <p>{props.x + props.y +props.z}</p>
+    </>
+  )
+}
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div className="App">
+      <Header course={course} />
+      <Content content={part1} exercise={exercises1} />
+      <Content content={part2} exercise={exercises2} />
+      <Content content={part3} exercise={exercises3} />
+      <Total x={exercises1} y={exercises2} z={exercises3}/>
     </div>
   )
 }
 
-function App() {
-  const now = new Date()
-  return (
-    <div className="App">
-      <p>Hello World {now.toString()}</p>
-
-      <Hello name="Otim" />
-    </div>
-  );
-}
-
-export default App;
+export default App
