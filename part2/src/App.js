babeee3c7d148b7b7e141f8361610a93d8e45629
Otim Brian <otims.brian@gmail.com>
2023-04-15 02:49:14 +0300

    Finished Exercise 2.9

--- part2/src/App.js
+++ part2/src/App.js
@@ -6,12 +6,19 @@ const App = () => {
 	const [persons, setPersons] = useState([])
 	const [newName, setNewName] = useState('')
 	const [newPhoneNumber, setNewPhoneNumber] = useState('')
+	const [search, setSearch] = useState('')
 
 	const handleNameChange = event => {
 		event.preventDefault()
-
 		setNewName(event.target.value)
 	}
+
+	const personToDisplay =
+		search === ''
+			? persons
+			: persons.filter(person =>
+				person.name.toLowerCase().includes(search.toLowerCase())
+			)
 
 	const handleNameSubmit = event => {
 		event.preventDefault()
@@ -29,14 +36,26 @@ const App = () => {
 
 	const handlePhoneNumberChange = event => {
 		event.preventDefault()
-
 		setNewPhoneNumber(event.target.value)
 	}
+
+	const handleSerchChange = event => {
+		event.preventDefault()
+		setSearch(event.target.value)
+	}
+
 	return (
 		<div className='App'>
 			<h2>Phonebook</h2>
+			<div>
+				<h2>Filter Contacts By Name</h2>
+				<form>
+					Filter by <input value={search} onChange={handleSerchChange} />
+				</form>
+			</div>
 			<form onSubmit={handleNameSubmit}>
 				<div>
+					<h2>Add New Contact</h2>
 					Name: <input onChange={handleNameChange} value={newName} />
 					PhoneNumber:{' '}
 					<input onChange={handlePhoneNumberChange} value={newPhoneNumber} />
@@ -46,7 +65,7 @@ const App = () => {
 				</div>
 			</form>
 			<h2>Numbers</h2>
-			<Contact persons={persons} />
+			<Contact persons={personToDisplay} />
 		</div>
 	)
 }
